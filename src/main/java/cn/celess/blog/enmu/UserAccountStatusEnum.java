bc20173084dbb722fed6aaa53bd14716eb46db4d
禾几海 <zh56462271@qq.com>
2020-05-23 00:16:08 +0800

    数据表改动

---
+++ src/main/java/cn/celess/blog/enmu/UserAccountStatusEnum.java
@@ -0,0 +1,41 @@
+package cn.celess.blog.enmu;
+
+import lombok.Data;
+
+/**
+ * @Author: 小海
+ * @Date: 2020-05-22 21:32
+ * @Desc:
+ */
+public enum UserAccountStatusEnum {
+
+    /**
+     * 账户正常
+     */
+    NORMAL(0, "正常"),
+    /**
+     * 账户被锁定
+     */
+    LOCKED(1, "锁定"),
+    /**
+     * 账户被删除
+     */
+    DELETED(2, "已删除");
+
+
+    private final int code;
+    private final String desc;
+
+    private UserAccountStatusEnum(int code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+}
