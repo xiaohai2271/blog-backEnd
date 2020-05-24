190e1624ca1ec3d1efe185e6a9329e36c04706a6
禾几海 <zh56462271@qq.com>
2020-05-25 00:20:36 +0800

    dao层修改  单元测试

--- src/main/java/cn/celess/blog/entity/PartnerSite.java
+++ src/main/java/cn/celess/blog/entity/PartnerSite.java
@@ -19,6 +19,12 @@ public class PartnerSite {
 
     private Boolean open;
 
+    private String iconPath;
+
+    private String desc;
+
+    private boolean delete;
+
     public PartnerSite() {
     }
 
