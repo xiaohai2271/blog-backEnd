6eb7d01875743a22d3f87a2cad5cccdddaf5353d
禾几海 <zh56462271@qq.com>
2020-08-14 17:14:17 +0800

    style:修改sql格式

--- src/main/resources/sql/schema_h2.sql
+++ src/main/resources/sql/schema_h2.sql
@@ -9,7 +9,7 @@ drop table if exists links;
 drop table if exists visitor;
 drop table if exists web_update;
 
--- 用户表
+--  用户表
 CREATE TABLE `user`
 (
     `u_id`                   int         not null primary key auto_increment,
@@ -17,16 +17,16 @@ CREATE TABLE `user`
     `u_pwd`                  varchar(40) not null comment '密码',
     `u_email_status`         boolean              default false comment '邮箱验证状态',
     `u_avatar`               varchar(255)         default null comment '用户头像',
-    `u_desc`                 tinytext  default null comment '用户的描述',
+    `u_desc`                 tinytext             default null comment '用户的描述',
     `u_recently_landed_time` datetime             default null comment '最近的登录时间',
-    `u_display_name`         varchar(30)  default null comment '展示的昵称',
+    `u_display_name`         varchar(30)          default null comment '展示的昵称',
     `u_role`                 varchar(40) not null default 'user' comment '权限组',
     `status`                 tinyint(1)  not null default 0 comment '账户状态',
     unique key `uni_user_id` (`u_id`),
     unique key `uni_user_email` (`u_email`)
 );
 
--- 标签和分类表
+--  标签和分类表
 CREATE TABLE `tag_category`
 (
     `t_id`        bigint(20) primary key auto_increment,
@@ -34,28 +34,30 @@ CREATE TABLE `tag_category`
     `is_category` boolean      not null default true,
     `is_delete`   boolean      not null default false comment '该数据是否被删除'
 );
---文章表
+
+--  文章表
 CREATE TABLE `article`
 (
     `a_id`             bigint(20) primary key auto_increment,
-    `a_title`          varchar(255)  not null unique comment '文章标题',
-    `a_summary`        varchar(255)  not null comment '文章摘要',
-    `a_md_content`     longtext  not null comment '文章Markdown内容',
-    `a_url`            tinytext         default null comment '转载文章的原文链接',
-    `a_author_id`      int     not null comment '作者id',
-    `a_is_original`    boolean          default true comment '文章是否原创',
-    `a_reading_number` int              default 0 comment '文章阅读数',
-    `a_like`           int              default 0 comment '文章点赞数',
-    `a_dislike`        int              default 0 comment '文章不喜欢数',
-    `a_category_id`    bigint  not null comment '文章分类id',
-    `a_publish_date`   datetime         default CURRENT_TIMESTAMP comment '文章发布时间',
-    `a_update_date`    datetime         default null comment '文章的更新时间',
-    `a_is_open`        boolean          default true comment '文章是否可见',
-    `is_delete`        boolean not null default false comment '该数据是否被删除',
+    `a_title`          varchar(255) not null unique comment '文章标题',
+    `a_summary`        varchar(255) not null comment '文章摘要',
+    `a_md_content`     longtext     not null comment '文章Markdown内容',
+    `a_url`            tinytext              default null comment '转载文章的原文链接',
+    `a_author_id`      int          not null comment '作者id',
+    `a_is_original`    boolean               default true comment '文章是否原创',
+    `a_reading_number` int                   default 0 comment '文章阅读数',
+    `a_like`           int                   default 0 comment '文章点赞数',
+    `a_dislike`        int                   default 0 comment '文章不喜欢数',
+    `a_category_id`    bigint       not null comment '文章分类id',
+    `a_publish_date`   datetime              default CURRENT_TIMESTAMP comment '文章发布时间',
+    `a_update_date`    datetime              default null comment '文章的更新时间',
+    `a_is_open`        boolean               default true comment '文章是否可见',
+    `is_delete`        boolean      not null default false comment '该数据是否被删除',
     foreign key (a_category_id) references tag_category (t_id),
     foreign key (a_author_id) references user (u_id)
 );
---文章标签表
+
+-- 文章标签表
 CREATE TABLE `article_tag`
 (
     `at_id` bigint(20) primary key auto_increment,
@@ -65,12 +67,12 @@ CREATE TABLE `article_tag`
     foreign key (t_id) references tag_category (t_id)
 );
 
---评论/留言表
+-- 评论/留言表
 CREATE TABLE `comment`
 (
     `co_id`             bigint(20) primary key auto_increment,
     `co_page_path`      varchar(255) not null comment '评论/留言的页面',
-    `co_content`        text  not null comment '评论/留言内容',
+    `co_content`        text         not null comment '评论/留言内容',
     `co_date`           datetime     not null comment '评论/留言的日期',
     `co_status`         tinyint      not null default 0 comment '评论的状态',
     `co_pid`            bigint       not null default -1 comment '评论/留言的父id',
@@ -79,20 +81,22 @@ CREATE TABLE `comment`
     foreign key (co_from_author_id) references user (u_id),
     foreign key (co_to_author_id) references user (u_id)
 );
--- 友站表
+
+--  友站表
 CREATE TABLE `links`
 (
     `l_id`           bigint(20) primary key auto_increment,
-    `l_name`         varchar(255)  not null comment '友站名称',
+    `l_name`         varchar(255)        not null comment '友站名称',
     `l_is_open`      boolean                      default true comment '是否公开',
     `l_url`          varchar(255) unique not null comment '首页地址',
     `l_icon_path`    varchar(255)        not null comment '友链的icon地址',
-    `l_desc`         varchar(255)  not null comment '友链的说明描述',
+    `l_desc`         varchar(255)        not null comment '友链的说明描述',
     `is_delete`      boolean             not null default false comment '该数据是否被删除',
     `l_email`        varchar(255) comment '网站管理员的邮箱',
     `l_notification` boolean                      default false comment '是否通知了'
 );
--- 访客表
+
+--  访客表
 CREATE TABLE `visitor`
 (
     `v_id`         bigint(20) primary key auto_increment,
@@ -102,13 +106,13 @@ CREATE TABLE `visitor`
     `is_delete`    boolean      not null default false comment '该数据是否被删除'
 );
 
---更新内容表
+-- 更新内容表
 CREATE TABLE `web_update`
 (
     `wu_id`     int primary key auto_increment,
-    `wu_info`   varchar(255)  not null comment '更新内容',
-    `wu_time`   datetime not null comment '更新时间',
-    `is_delete` boolean  not null default false comment '该数据是否被删除'
+    `wu_info`   varchar(255) not null comment '更新内容',
+    `wu_time`   datetime     not null comment '更新时间',
+    `is_delete` boolean      not null default false comment '该数据是否被删除'
 );
 
 CREATE VIEW articleView
