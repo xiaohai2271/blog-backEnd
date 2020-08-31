6eabb3ab2248f66244889a80f57a3e8ade774797
禾几海 <zh56462271@qq.com>
2020-08-31 10:10:21 +0800

    refactor: 精简代码

--- src/main/java/cn/celess/blog/controller/LinksController.java
+++ src/main/java/cn/celess/blog/controller/LinksController.java
@@ -11,8 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author : xiaohai
@@ -46,14 +46,7 @@ public class LinksController {
 
     @GetMapping("/links")
     public Response allForOpen() {
-        List<PartnerSite> sites = new ArrayList<>();
-        for (PartnerSite p : partnerSiteService.findAll()) {
-            if (p.getOpen()) {
-                //隐藏open字段
-                p.setOpen(null);
-                sites.add(p);
-            }
-        }
+        List<PartnerSite> sites = partnerSiteService.findAll().stream().peek(partnerSite -> partnerSite.setOpen(null)).collect(Collectors.toList());
         return Response.success(sites);
     }
 
